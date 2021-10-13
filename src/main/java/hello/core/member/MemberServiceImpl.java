8cc6e2e1f2ea81e4f134899fa47fb99455226b97
hyeri0903 <hyerijung0903@gmail.com>
2021-10-13 11:25:32 +0900

    create AppConfig and modify constructor injection(Member, Order)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -8,7 +8,12 @@ import org.w3c.dom.ls.LSOutput;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //생성자 주입
+    private final MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     @Override
     public void join(Member member) {
